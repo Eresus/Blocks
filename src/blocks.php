ea0d3f9cdbf1105dfea7e226280559a333a84c2f
Михаил Красильников <m.krasilnikov@yandex.ru>
2011-09-01 05:28:04 +0000

    Удалён неиспользуемый метод adminRenderContent

--- src/blocks.php
+++ src/blocks.php
@@ -454,54 +454,6 @@ class Blocks extends Plugin
 	}
 	//-----------------------------------------------------------------------------
 
-	private function adminRenderContent()
-	{
-	global $Eresus, $page;
-
-		$result = '';
-		if (!is_null(arg('id')))
-		{
-			$item = $Eresus->db->selectItem($this->table['name'], "`".$this->table['key']."` = '".arg('id', 'dbsafe')."'");
-			$page->title .= empty($item['caption'])?'':' - '.$item['caption'];
-		}
-		switch (true) {
-			case !is_null(arg('update')) && isset($this->table['controls']['edit']):
-				if (method_exists($this, 'update')) $result = $this->update(); else ErrorMessage(sprintf(errMethodNotFound, 'update', get_class($this)));
-			break;
-			case !is_null(arg('toggle')) && isset($this->table['controls']['toggle']):
-				if (method_exists($this, 'toggle')) $result = $this->toggle(arg('toggle', 'dbsafe')); else ErrorMessage(sprintf(errMethodNotFound, 'toggle', get_class($this)));
-			break;
-			case !is_null(arg('delete')) && isset($this->table['controls']['delete']):
-				if (method_exists($this, 'delete')) $result = $this->delete(arg('delete', 'dbsafe')); else ErrorMessage(sprintf(errMethodNotFound, 'delete', get_class($this)));
-			break;
-			case !is_null(arg('up')) && isset($this->table['controls']['position']):
-				if (method_exists($this, 'up')) $result = $this->table['sortDesc']?$this->down(arg('up', 'dbsafe')):$this->up(arg('up', 'dbsafe')); else ErrorMessage(sprintf(errMethodNotFound, 'up', get_class($this)));
-			break;
-			case !is_null(arg('down')) && isset($this->table['controls']['position']):
-				if (method_exists($this, 'down')) $result = $this->table['sortDesc']?$this->up(arg('down', 'dbsafe')):$this->down(arg('down', 'dbsafe')); else ErrorMessage(sprintf(errMethodNotFound, 'down', get_class($this)));
-			break;
-			case !is_null(arg('id')) && isset($this->table['controls']['edit']):
-				if (method_exists($this, 'adminEditItem')) $result = $this->adminEditItem(); else ErrorMessage(sprintf(errMethodNotFound, 'adminEditItem', get_class($this)));
-			break;
-			case !is_null(arg('action')):
-				switch (arg('action')) {
-					case 'create': if (isset($this->table['controls']['edit']))
-						if (method_exists($this, 'adminAddItem')) $result = $this->adminAddItem();
-						else ErrorMessage(sprintf(errMethodNotFound, 'adminAddItem', get_class($this)));
-					break;
-					case 'insert':
-						if (method_exists($this, 'insert')) $result = $this->insert();
-						else ErrorMessage(sprintf(errMethodNotFound, 'insert', get_class($this)));
-					break;
-				}
-			break;
-			default:
-				if (!is_null(arg('section'))) $this->table['condition'] = "`section`='".arg('section', 'int')."'";
-				$result = $page->renderTable($this->table);
-		}
-		return $result;
-	}
-
 	/**
 	 * @see Plugin::install()
 	 */
